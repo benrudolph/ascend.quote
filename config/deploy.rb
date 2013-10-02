0ea7f70fcea77fb48faf19d547497666acee0d4e
Ben Rudolph <rudolphben@gmail.com>
2013-10-02 17:25:15 -0500

    Diff deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,4 +40,4 @@ task :simlink_database, :roles => :app do
   run "ln -s ~/quote-it.yml /var/www/quote-it/current/config/database.yml"
 end
 
-after "deploy", :simlink_database
+after "deploy:update_code", :simlink_database
